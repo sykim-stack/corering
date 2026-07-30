574d75810f677584950ecc03266243187973d2b3
sykim-stack <sykim.spotiq@gmail.com>
2026-07-30 23:00:28 +0900

    코어링 구글 등록

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,6 @@ import type { MetadataRoute } from 'next';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://brainpool.vercel.app/sitemap.xml',
+    sitemap: 'https://corering.vercel.app/sitemap.xml',
   };
 }
